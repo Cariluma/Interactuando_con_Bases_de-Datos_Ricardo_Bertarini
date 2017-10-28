c4f743f37853a2cc123d569abe72dd370fa57048
Ricardo_Bertarini <bertarini@gmail.com>
2017-10-28 08:56:32 -0300

    Cambios PHP base de datos

--- NextUcodBaseAgenda_versionPHP/codBase/server/create_user.php
+++ NextUcodBaseAgenda_versionPHP/codBase/server/create_user.php
@@ -1,42 +1,20 @@
 <?php
 
-require('../server/conector.php');
+require('conector.php');
 
 $con = new ConectorBD();
+if ($con->initConexion('agenda');
 
-if ($con->initConexion()=='OK'){
-    $datos['nombres'] = "'Ricardo Bertarini'";
-    $datos['correo'] = "'ricardo@gmail.com'";
-    $datos['password'] = "'".password_hash("123",PASSWORD_DEFAULT)."'";
-    $datos['fecnac'] = "'1990/05/19'";
+$hash = password_hash('1234', PASSWORD_DEFAULT);
 
-    if ($con->insertData('usuarios', $datos)) {
-      echo "Se han registrado los datos correctamente";
-    }else echo "Se ha producido un error en la actualización";
+$data['username']= '"ricardob"';
+$data['password']= '"'.$hash.'"';
+$data['nombre']='"Ricardo Bertarini"';
+$data['fecha_nacimiento']='"1975-11-01"';
 
-    $datos['nombres'] = "'Lucas Bertarini'";
-    $datos['correo'] = "'lucas@gmail.com'";
-    $datos['password'] = "'".password_hash("123",PASSWORD_DEFAULT)."'";
-    $datos['fecnac'] = "'1997/05/19'";
-
-    if ($con->insertData('usuarios', $datos)) {
-      echo "Se han registrado los datos correctamente";
-    }else echo "Se ha producido un error en la actualización";
-
-    $datos['nombres'] = "'Martin Bertarini'";
-    $datos['correo'] = "'martin@gmail.com'";
-    $datos['password'] = "'".password_hash("123",PASSWORD_DEFAULT)."'";
-    $datos['fecnac'] = "'1986/01/19'";
-
-    if ($con->insertData('usuarios', $datos)) {
-      echo "Se han registrado los datos correctamente";
-    }else echo "Se ha producido un error en la actualización";
+$con->insertData('usuarios', $data);
 
 
-    $con->cerrarConexion();
-}else {
-    echo "Se presentó un error en la conexión";
-}
 
 
- ?>
+?>
